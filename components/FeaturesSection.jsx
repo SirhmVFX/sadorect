b23951dff68b191e689701d320b38d818576b755
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-04-29 18:53:45 +0100

    done

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -17,45 +17,42 @@ function FeaturesSection() {
             <span className="font-bold text-primarycolor">sadorect</span>
           </h1>
           <p className="text-gray-500 text-lg text-[11px] leading-tight md:text-base  md:text-center">
-            Our service are tailored to meet unique needs and challenges of each
-            brand, and are designed to provide the right brand identity
-            necessary to help brands reach their full potential
+            As a business owner or manager, you are no doubt always looking for
+            ways to improve efficiency, reduce costs, and stay ahead of the
+            competition. One way to achieve these goals is by leveraging the
+            expertise of an IT consulting firm.
           </p>
         </div>
 
-        <div className="py-10 md:flex-row flex-col  flex gap-4 md:gap-8 justify-center">
+        <div className="md:py-10 py-4  flex gap-4 md:gap-8 justify-center">
           <div
             onClick={() => setFeatures("info1")}
             className="p-2 md:p-4 text-[12px] md:text-base transition-transform duration-300 transform cursor-pointer hover:scale-110 bg-gray-50 rounded-lg border border-primarycolor "
           >
-            <p>Brand Strategy and Positioning</p>
+            <p>Software Support</p>
           </div>
 
           <div
             onClick={() => setFeatures("info2")}
             className="md:p-4 p-2 text-[12px] md:text-base transition-transform duration-300 transform cursor-pointer hover:scale-110 bg-gray-50 rounded-lg border border-primarycolor "
           >
-            <p>Digital Presence and Web Development</p>
+            <p>I.T. Consulting</p>
           </div>
           <div
             onClick={() => setFeatures("info3")}
             className="md:p-4 p-2 text-[12px] md:text-base transition-transform duration-300 transform cursor-pointer hover:scale-110  bg-gray-50 rounded-lg border border-primarycolor "
           >
-            <p>Brand Identity Development</p>
+            <p>Web Hosting and Management</p>
           </div>
         </div>
         {feature === "info2" ? (
           <div className="md:flex gap-4 items-center">
             <div className="bg-white p-6 md:w-1/2 flex items-start flex-col gap-4 md:gap-8 rounded-3xl">
-              <h1 className="font-bold text-xl md:text-2xl">
-                Digital Presence and Web Development
-              </h1>
+              <h1 className="font-bold text-xl md:text-2xl">I.T. Consulting</h1>
               <p className="md:text-base text-[11px]">
-                Sadorect can optimize your digital presence by designing and
-                developing a user-friendly website that reflects your brand
-                identity. They can also assist with search engine optimization
-                (SEO), content strategy, and user experience (UX) design to
-                enhance online visibility and engagement.
+                We provide advice and guidance to businesses on how to use
+                information technology in order to meet their business
+                objectives
               </p>
 
               <div className="flex gap-3 flex-col">
@@ -64,19 +61,19 @@ function FeaturesSection() {
                 <div className="flex items-center gap-2">
                   <FaCheck className=" md:text-base text-[11px] text-primarycolor" />
                   <p className="text-[11px] md:text-base">
-                    Differentiation and Competitive Advantage:
+                    Differentiation and Competitive Advantage
                   </p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck className=" md:text-base text-[11px]  text-primarycolor" />
                   <p className="text-[11px] md:text-base">
-                    Consistent Brand Experience:
+                    Consistent Brand Experience
                   </p>
                 </div>
                 <div className="flex items-center gap-2">
                   <FaCheck className=" md:text-base text-[11px]  text-primarycolor" />
                   <p className="text-[11px] md:text-base">
-                    Targeted Marketing and Communication:
+                    Targeted Marketing and Communication
                   </p>
                 </div>
               </div>
@@ -97,14 +94,11 @@ function FeaturesSection() {
           <div className="flex gap-4 items-center">
             <div className="bg-white p-6 md:w-1/2 flex items-start flex-col gap-4 md:gap-8 rounded-3xl">
               <h1 className="font-bold text-xl md:text-2xl">
-                Brand Identity Development
+                Web Hosting and Management
               </h1>
               <p className="md:text-base text-[11px]">
-                We can create or refresh your brand identity, including
-                designing a compelling logo, selecting brand colors and fonts,
-                and developing brand guidelines. A strong brand identity helps
-                create a cohesive and memorable brand image across all
-                touchpoints.{" "}
+                We help you manage your online presence to ensure you are
+                visible and always available when your clients need you online.
               </p>
 
               <div className="flex gap-3 flex-col">
@@ -146,14 +140,11 @@ function FeaturesSection() {
           <div className="flex gap-4 items-center">
             <div className="bg-white p-6 md:w-1/2 flex items-start flex-col gap-4 md:gap-8 rounded-3xl">
               <h1 className="font-bold text-xl md:text-2xl">
-                Brand Strategy and Positioning
+                Software and Support
               </h1>
               <p className="md:text-base text-[11px]">
-                Sadorect can work with you to develop a clear brand strategy,
-                defining your brand&apos;s unique positioning, target audience,
-                value proposition, and messaging. They help identify what sets
-                your business apart from competitors and how to effectively
-                communicate this to your audience.
+                You are welcome to explore our technical support to assist with
+                issues you might be facing with your softwares{" "}
               </p>
 
               <div className="flex gap-3 flex-col">
